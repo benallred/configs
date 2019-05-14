fba58f0733e9bd7ade99c81f9152de1352c50cc5
Ben Allred <ben@bmallred.com>
2019-05-13 22:39:51 -0600

    Add more to initial config script; Add run instructions

--- config.ps1
+++ config.ps1
@@ -1,1 +1,19 @@
+$src = (Resolve-Path "$PSScriptRoot\..")
+
+git clone https://github.com/lukesampson/concfg.git $src\concfg
+. $src\concfg\bin\concfg.ps1 import vs-code-dark-plus -n
+. $src\concfg\bin\concfg.ps1 clean
+
+Install-Module posh-git
+Add-PoshGitToProfile -AllHosts
+
 git config --global --add include.path $PSScriptRoot\git\ben.gitconfig
+
+if (!(Test-Path $profile) -or !(Select-String "$src\\configs\\powershell\\profile.ps1" $profile))
+{
+	Add-Content -Path $profile -Value "`n. $src\configs\powershell\profile.ps1"
+}
+
+Update-Help
+
+start powershell
---
+++ readme.md
@@ -0,0 +1,7 @@
+# configs
+
+```
+Set-ExecutionPolicy RemoteSigned
+git clone https://github.com/benallred/configs.git C:\BenLocal\git\configs
+C:\BenLocal\git\configs\config.ps1
+```
