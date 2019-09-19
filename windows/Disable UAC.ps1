6e54a782faeb5ba80e06c257d1f730c43eea71f9
Ben Allred <ben@bmallred.com>
2019-09-19 16:20:09 -0600

    Windows: Set computer name and disable UAC

---
+++ windows/Disable UAC.ps1
@@ -0,0 +1,2 @@
+Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name ConsentPromptBehaviorAdmin -Value 0
+Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name PromptOnSecureDesktop -Value 0
--- windows/config.ps1
+++ windows/config.ps1
@@ -1,3 +1,5 @@
+Rename-Computer -NewName (Read-Host "Set computer name to")
+& '.\windows\Disable UAC.ps1'
 & '.\windows\Hide Folders Section in This PC.ps1'
 & '.\windows\Associate Extensionless Files with VS Code.ps1'
 Create-Shortcut -Target "$env:LocalAppData\Programs\Microsoft VS Code\Code.exe" -Link "$((New-Object -ComObject WScript.Shell).SpecialFolders.Item("sendto"))\nCode.lnk"
