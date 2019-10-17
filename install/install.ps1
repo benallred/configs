1292f91348fad84fb11f1c289d883d462c38729c
Ben Allred <ben@bmallred.com>
2019-10-17 10:10:02 -0600

    Install: Java, SBT, and Scala

--- install/install.ps1
+++ install/install.ps1
@@ -1,1 +1,5 @@
 iwr -useb get.scoop.sh | iex
+scoop bucket add java
+
+scoop install adopt8-hotspot -a 32bit # Java 1.8 JDK; Metals for VS Code does not work with 64-bit
+scoop install sbt scala # Scala
