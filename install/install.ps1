b1c7b9a07c3f180796c9aa1c7d3d4e3bd76997fe
Ben Allred <ben@bmallred.com>
2019-10-19 21:19:06 -0600

    Install: Sysinternals

--- install/install.ps1
+++ install/install.ps1
@@ -1,5 +1,8 @@
 iwr -useb get.scoop.sh | iex
+scoop bucket add extras
 scoop bucket add java
+
+scoop install sysinternals
 
 scoop install adopt8-hotspot -a 32bit # Java 1.8 JDK; Metals for VS Code does not work with 64-bit
 scoop install sbt scala # Scala
