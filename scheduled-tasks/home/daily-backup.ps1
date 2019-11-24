d19399d787a6e0ff9af88aa90d53b65f1ddda3ba
Ben Allred <ben@bmallred.com>
2019-11-23 22:20:37 -0700

    Scheduled Task: Home: Daily: Backup

---
+++ scheduled-tasks/home/daily-backup.ps1
@@ -0,0 +1,21 @@
+function Backup([string]$from, [string]$toBase) {
+    $weekday = Get-Date -Format "ddd"
+    $weekdayNumber = (Get-Date).DayOfWeek.value__
+    $to = "$toBase\$weekdayNumber $weekday"
+    $logFile = "$to.log"
+    Write-Output "Backing up $from to $to"
+    Write-Output "Writing log file to $logFile"
+    # /Z           = copy files in restartable mode
+    # /DCOPY:T     = COPY Directory Timestamps
+    # /MIR         = MIRror a directory tree (equivalent to /E plus /PURGE)
+    # /X           = report all eXtra files, not just those selected
+    # /NDL         = No Directory List - don't log directory names
+    # /NP          = No Progress - don't display percentage copied
+    # /UNILOG:file = output status to LOG file as UNICODE (overwrite existing log)
+    # /TEE         = output to console window, as well as the log file
+    StopOnError 4 { robocopy $from $to /Z /DCOPY:T /MIR /X /NDL /NP /UNILOG:"$logFile" /TEE }
+}
+
+Backup "C:\Ben" "J:\Backup - Daily\Ben"
+Backup "$env:UserProfile\OneDrive\Ben" "J:\Backup - Daily\OneDrive_Ben"
+Backup "$env:UserProfile\OneDrive\Music" "J:\Backup - Daily\OneDrive_Music"
--- scheduled-tasks/home/daily.ps1
+++ scheduled-tasks/home/daily.ps1
@@ -1,1 +1,13 @@
-dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- --playlist-id PLAYgY8SPtEWGh243j2fmgeCxnFtpbWwZd --download-folder "E:\Media\Church\TV\Book of Mormon Videos"
+function StopOnError([int]$MinimumErrorCode, [scriptblock]$ScriptBlock) {
+    Invoke-Command $ScriptBlock
+    if ($LastExitCode -ge $MinimumErrorCode) {
+        Read-Host
+        Exit $LastExitCode
+    }
+}
+
+StopOnError 1 {
+    dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- --playlist-id PLAYgY8SPtEWGh243j2fmgeCxnFtpbWwZd --download-folder "E:\Media\Church\TV\Book of Mormon Videos"
+}
+
+& $PSScriptRoot\daily-backup.ps1
