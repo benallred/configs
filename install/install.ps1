e454d0ab2c01baa7e28e5ceede57b38ff8a37216
Ben Allred <ben@bmallred.com>
2020-04-15 21:28:06 -0600

    Install: Office

--- install/install.ps1
+++ install/install.ps1
@@ -166,6 +166,37 @@ Block "Install Slack" {
     Test-ProgramInstalled Slack
 }
 
+Block "Install Office" {
+    # https://www.microsoft.com/en-in/download/details.aspx?id=49117
+    $downloadUrl = (iwr "https://www.microsoft.com/en-in/download/confirmation.aspx?id=49117" -useb | sls "https://download\.microsoft\.com/download/.+?/officedeploymenttool_.+?.exe").Matches.Value
+    iwr $downloadUrl -OutFile $env:tmp\officedeploymenttool.exe
+    . $env:tmp\officedeploymenttool.exe /extract:$env:tmp\officedeploymenttool /passive /quiet
+    while (!(Test-Path $env:tmp\officedeploymenttool\setup.exe)) { sleep -s 10 }
+    . $env:tmp\officedeploymenttool\setup.exe /configure $PSScriptRoot\OfficeConfiguration.xml
+    # TODO: Activate
+    #   Observed differences
+    #       Manual install and activation
+    #           Word > Account: Product Activated \ Microsoft Office Professional Plus 2019
+    #           cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" /dstatus
+    #               LICENSE NAME: Office 19, Office19ProPlus2019MSDNR_Retail edition
+    #               LICENSE DESCRIPTION: Office 19, RETAIL channel
+    #               LICENSE STATUS:  ---LICENSED---
+    #               Last 5 characters of installed product key: <correct>
+    #       Automated install, no activation
+    #           Word > Account: Activation Required \ Microsoft Office Professional 2019
+    #       Automated install, activation by filling in PIDKEY
+    #           Word > Account: Subscription Product \ Microsoft Office 365 ProPlus
+    #           cscript ... /dstatus
+    #               Other stuff about a grace period, even though in-product it says activated
+    #               Last 5 characters of installed product key: <different>
+    #   Next attempts:
+    #       1. <Product ID="ProPlus2019Volume>, fill in PIDKEY
+    #       2. https://support.office.com/en-us/article/Change-your-Office-product-key-d78cf8f7-239e-4649-b726-3a8d2ceb8c81#ID0EABAAA=Command_line
+    #       3. Manual activation
+} {
+    Test-ProgramInstalled "Microsoft Office Professional Plus 2019 - en-us"
+}
+
 InstallFromScoopBlock Sysinternals sysinternals
 
 function InstallFromGitHubBlock([string]$User, [string]$Repo, [scriptblock]$AfterClone) {
