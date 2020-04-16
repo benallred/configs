244206a87ff60027d3d31b363d4384d3faee799f
Ben Allred <ben@bmallred.com>
2020-04-15 21:28:30 -0600

    Windows: Apps: Uninstall some prepackaged apps

--- windows/apps.ps1
+++ windows/apps.ps1
@@ -1,3 +1,11 @@
+function UninstallBlock([string]$AppName) {
+    Block "Uninstall Appx package $AppName" {
+        Get-AppxPackage $AppName | Remove-AppxPackage
+    } {
+        !(Get-AppxPackage $AppName)
+    }
+}
+
 FirstRunBlock "Configure OneNote" {
     Write-ManualStep "Start OneNote notebooks syncing"
     start onenote:
@@ -7,3 +15,13 @@ FirstRunBlock "Connect phone" {
     Write-ManualStep "Connect phone"
     start ms-phone:
 }
+
+UninstallBlock Microsoft.BingWeather
+UninstallBlock Microsoft.GetHelp
+UninstallBlock Microsoft.Getstarted # Tips
+UninstallBlock Microsoft.OneConnect # Mobile Plans
+UninstallBlock Microsoft.MicrosoftSolitaireCollection
+UninstallBlock Microsoft.WindowsFeedbackHub
+UninstallBlock Microsoft.MicrosoftOfficeHub
+UninstallBlock Microsoft.WindowsMaps
+UninstallBlock SpotifyAB.SpotifyMusic
