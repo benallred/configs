7aab16291589da873d6ccd430df961ee091d8457
Ben Allred <ben@bmallred.com>
2020-04-26 15:17:15 -0600

    Scheduled Task: Home: Daily: YouTubeToPlex Studio C YouTube Exclusives

--- scheduled-tasks/home/daily.ps1
+++ scheduled-tasks/home/daily.ps1
@@ -8,6 +8,7 @@ function StopOnError([int]$MinimumErrorCode, [scriptblock]$ScriptBlock) {
 
 StopOnError 1 {
     dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- --playlist-id PLAYgY8SPtEWGh243j2fmgeCxnFtpbWwZd --download-folder "E:\Media\Church\TV\Book of Mormon Videos"
+    dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- --playlist-id PLGVpxD1HlmJ-OuDJlytqoxj5oEme6RSVz --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 0
     dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- --playlist-id PLGVpxD1HlmJ-sdaH6yq_EC7248oXq1i6I --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 10
 }
 
