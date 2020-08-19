ce774ac248f72eee3834d24010ac420937caf092
Ben Allred <ben@bmallred.com>
2020-08-18 21:08:23 -0600

    Scheduled Task: Home: Daily: YouTubeToPlex CLI update

--- scheduled-tasks/home/daily.ps1
+++ scheduled-tasks/home/daily.ps1
@@ -7,9 +7,9 @@ function StopOnError([int]$MinimumErrorCode, [scriptblock]$ScriptBlock) {
     }
 }
 
-StopOnError 1 { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- --playlist-id PLAYgY8SPtEWGh243j2fmgeCxnFtpbWwZd --download-folder "E:\Media\Church\TV\Book of Mormon Videos" }
-StopOnError 1 { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- --playlist-id PLGVpxD1HlmJ-OuDJlytqoxj5oEme6RSVz --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 0 }
-StopOnError 1 { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- --playlist-id PLGVpxD1HlmJ-sdaH6yq_EC7248oXq1i6I --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 10 }
-StopOnError 1 { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- --playlist-id PLGVpxD1HlmJ8GxbiW7jdGzPlKJeO5sz3B --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 11 }
+StopOnError 1 { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLAYgY8SPtEWGh243j2fmgeCxnFtpbWwZd --download-folder "E:\Media\Church\TV\Book of Mormon Videos" }
+StopOnError 1 { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ-OuDJlytqoxj5oEme6RSVz --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 0 }
+StopOnError 1 { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ-sdaH6yq_EC7248oXq1i6I --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 10 }
+StopOnError 1 { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ8GxbiW7jdGzPlKJeO5sz3B --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 11 }
 
 & $PSScriptRoot\daily-backup.ps1
