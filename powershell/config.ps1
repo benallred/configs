4698eb4b0d31fe7d164fd8c888f6fc52b91cad84
Ben Allred <ben@bmallred.com>
2020-08-25 16:37:17 -0600

    PowerShell: Install and configure Windows Terminal

--- powershell/config.ps1
+++ powershell/config.ps1
@@ -28,6 +28,19 @@ Block "profile.ps1" {
     (Test-Path $profile) -and (Select-String "$($PSScriptRoot -replace "\\", "\\")\\profile.ps1" $profile) # <original> is regex, <substitute> is PS string
 }
 
+Block "Install Windows Terminal" {
+    Write-ManualStep "Install Windows Terminal"
+    start ms-windows-store://pdp/?ProductId=9n0dx20hk701
+} {
+    Get-AppxPackage -Name Microsoft.WindowsTerminal
+}
+
+Block "Configure Windows Terminal" {
+    $settingsDir = "$env:LocalAppData\Packages\$((Get-AppxPackage -Name Microsoft.WindowsTerminal).PackageFamilyName)\LocalState"
+    while (!(Test-Path $settingsDir)) { sleep -s 10 }
+    Copy-Item $PSScriptRoot\settings.json $settingsDir\settings.json
+}
+
 FirstRunBlock "Update PS help" {
     if (!(& $configure $forTest)) {
         Update-Help -ErrorAction Ignore
