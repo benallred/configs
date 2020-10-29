1e8ab736e303eda7bcf677a4c6fbed3356060bfc
Ben Allred <ben@bmallred.com>
2020-10-29 12:47:22 -0600

    Scheduled Task: Speedtest: Recommend rebooting router when daily average drops below 100 Mbps

--- scheduled-tasks/home/speedtest.ps1
+++ scheduled-tasks/home/speedtest.ps1
@@ -8,6 +8,11 @@ $json = ConvertFrom-Json (speedtest -f json)
 $megaBitsPerSecond = ($json.download.bytes / $json.download.elapsed * 1000 / 125000)
 Add-Content C:\BenLocal\speedtest.log $megaBitsPerSecond
 $measurements = Get-Content C:\BenLocal\speedtest.log
+$megaBitsPerSecond_AvgDay = $measurements | select -Last 3 | Measure-Object -Average | select -ExpandProperty Average
 $megaBitsPerSecond_AvgWeek = $measurements | select -Last 21 | Measure-Object -Average | select -ExpandProperty Average
 $megaBitsPerSecond_AvgAll = $measurements | Measure-Object -Average | select -ExpandProperty Average
 New-BurntToastNotification -Header (New-BTHeader -Id ec9f5700-36d1-4f18-bb09-2808e35ff33a -Title "Speedtest (Mbps)") -Text "Last: $($megaBitsPerSecond.ToString("N0")); Week: $($megaBitsPerSecond_AvgWeek.ToString("N0")); All: $($megaBitsPerSecond_AvgAll.ToString("N0"))" -Button (New-BTButton -Dismiss)
+
+if ($megaBitsPerSecond_AvgDay -lt 100) {
+    New-BurntToastNotification -Header (New-BTHeader -Id ec9f5700-36d1-4f18-bb09-2808e35ff33a -Title "Speedtest (Mbps)") -Text "Recommend rebooting router; Day: $($megaBitsPerSecond_AvgDay.ToString("N0"))" -Button (New-BTButton -Content "Reboot router" -Arguments http://192.168.0.1/RouterStatus.htm)
+}
