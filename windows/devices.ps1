37a8c10c6ebe936f39be61af3d5ac0066d739643
Ben Allred <ben@bmallred.com>
2021-02-24 13:08:00 -0700

    Windows: Devices: Touchpad: Four finger gestures

--- windows/devices.ps1
+++ windows/devices.ps1
@@ -8,3 +8,17 @@ FirstRunBlock "Devices > Printers & scanners > Add a printer or scanner > The pr
 Block "Devices > Touchpad > Three-finger gestures > Taps = Middle mouse button" {
     Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name ThreeFingerTapEnabled -Value 4
 } -RequiresReboot
+Block "Devices > Touchpad > Four-finger gestures" {
+    Write-Output "Devices > Touchpad > Four-finger gestures > Swipes = Custom"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerSlideEnabled -Value 0xffff
+    Write-Output "Devices > Touchpad > Related settings > Advanced gesture configuration > Configure your four finger gestures > Up = Volume up"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerUp -Value 16
+    Write-Output "Devices > Touchpad > Related settings > Advanced gesture configuration > Configure your four finger gestures > Down = Volume down"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerDown -Value 17
+    Write-Output "Devices > Touchpad > Related settings > Advanced gesture configuration > Configure your four finger gestures > Left = Next track"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerLeft -Value 14
+    Write-Output "Devices > Touchpad > Related settings > Advanced gesture configuration > Configure your four finger gestures > Right = Previous track"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerRight -Value 15
+    Write-Output "Devices > Touchpad > Four-finger gestures > Taps = Play/pause"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerTapEnabled -Value 3
+} -RequiresReboot
