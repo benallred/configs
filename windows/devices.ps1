6762d13c9ff70b9ec1a2887ac11b5b7ecb449f71
Ben Allred <ben@bmallred.com>
2021-04-14 22:15:41 -0600

    Windows: Devices: Add printer: Skip when testing

--- windows/devices.ps1
+++ windows/devices.ps1
@@ -1,5 +1,5 @@
-FirstRunBlock "Devices > Printers & scanners > Add a printer or scanner > The printer that I want isn't listed" {
-    if (!(Configured $forHome)) {
+if (!(Configured $forHome) -and !(Configured $forTest)) {
+    FirstRunBlock "Devices > Printers & scanners > Add a printer or scanner > The printer that I want isn't listed" {
         Write-ManualStep "Select a shared printer by name = \\{Server}\{Printer}"
         rundll32 printui.dll PrintUIEntry /im
         WaitWhileProcess rundll32
