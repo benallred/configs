4fb74dd8b8aa5617b09919fdbd8428ae6d78c70b
Ben Allred <ben@bmallred.com>
2021-05-05 14:31:32 -0600

    Work: Install: Cricut Design Space

--- work/config.ps1
+++ work/config.ps1
@@ -44,6 +44,13 @@ if ((Configured $forWork) -or (Configured $forTest)) {
         Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Office\Outlook\Addins\MimecastServicesForOutlook.AddinModule" -Name LoadBehavior -Value 2
     }
 
+    Block "Install Cricut Design Space" {
+        $fileName = (iwr https://s3-us-west-2.amazonaws.com/staticcontent.cricut.com/a/software/win32-native/latest.json | ConvertFrom-Json).rolloutInstallFile
+        Download-File https://staticcontent.cricut.com/a/software/win32-native/$fileName $env:tmp\$fileName
+        . $env:tmp\$fileName
+        DeleteDesktopShortcut "Cricut Design Space"
+    }
+
     InstallFromScoopBlock "AWS CLI" aws {
         Add-Content -Path $profile {
             Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
