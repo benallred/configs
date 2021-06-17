6b9dd8035a3430384df08775a05e9dd3d0767d76
Ben Allred <ben@bmallred.com>
2021-06-17 00:13:59 -0600

    Scheduled Task: Home: Daily: Backup before doing other things

--- scheduled-tasks/home/daily.ps1
+++ scheduled-tasks/home/daily.ps1
@@ -1,5 +1,7 @@
 . $PSScriptRoot\..\functions.ps1
 
+& $PSScriptRoot\daily-backup.ps1
+
 StopOnError 8 {
     $saveFolder = "C:\BenEx\Humor\Dilbert"
     dotnet run -p $git\DilbertImageDownloader\ -- --save-folder=$saveFolder
@@ -8,5 +10,3 @@ StopOnError 8 {
 StopOnError { dotnet run -p $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLAYgY8SPtEWGh243j2fmgeCxnFtpbWwZd --download-folder "E:\Media\Church\TV\Book of Mormon Videos" }
 StopOnError { dotnet run -p $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ-OuDJlytqoxj5oEme6RSVz --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 0 }
 StopOnError { dotnet run -p $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ8pQN7WMrWqWyqlsFlzbl-- --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 12 }
-
-& $PSScriptRoot\daily-backup.ps1
