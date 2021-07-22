9e178f46bc9b9ebd9d9b8c6d8708301c3caf9130
Ben Allred <ben@bmallred.com>
2021-07-22 15:03:18 -0600

    PowerShell: winget: Argument completer

--- powershell/config.ps1
+++ powershell/config.ps1
@@ -29,6 +29,22 @@ Block "Configure profile.ps1" {
     (Test-Path $profile) -and (Select-String "$($PSScriptRoot -replace "\\", "\\")\\profile.ps1" $profile) # <original> is regex, <substitute> is PS string
 }
 
+Block "Configure winget" {
+    Add-Content -Path $profile {
+        Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+            param($wordToComplete, $commandAst, $cursorPosition)
+            [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+            $Local:word = $wordToComplete.Replace('"', '""')
+            $Local:ast = $commandAst.ToString().Replace('"', '""')
+            winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
+        }
+    }
+} {
+    (Test-Path $profile) -and (Select-String "Register-ArgumentCompleter.*winget" $profile)
+}
+
 Block "Install scoop" {
     iwr get.scoop.sh | iex
 } {
