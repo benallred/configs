aef48b5d8654ee6f6a3bd4f4e7e7001d97d943ae
Ben Allred <ben@bmallred.com>
2021-08-23 18:36:52 -0600

    Scheduled Task: Work: Daily

--- scheduled-tasks/work/config.ps1
+++ scheduled-tasks/work/config.ps1
@@ -1,1 +1,2 @@
+ScheduledTaskBlock "Scheduled Tasks: Work: Daily" "$PSScriptRoot\daily.ps1" "Daily (Work)"
 ScheduledTaskBlock "Scheduled Tasks: Work: Monthly" "$PSScriptRoot\monthly.ps1" "Monthly (Work)"
---
+++ scheduled-tasks/work/daily.ps1
@@ -0,0 +1,3 @@
+. $PSScriptRoot\..\functions.ps1
+
+. $cricutConfigs\cricut-config.ps1
