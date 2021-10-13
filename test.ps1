73ddb11d5d713d1970b752d91b2d0b34e41e0144
Ben Allred <ben@bmallred.com>
2021-10-13 16:54:41 -0600

    Test: Move to Windows 11
    Requires TPM which requires a generation 2 VM

--- test.ps1
+++ test.ps1
@@ -23,9 +23,13 @@ if ($Action -eq "Start") {
     }
     else {
         Write-Output "Creating VM '$vmName'"
-        $vm = New-VM -Name $vmName -NewVHDPath $vhdx -NewVHDSizeBytes 60GB -MemoryStartupBytes 4GB -SwitchName (Get-VMSwitch -Name "Default Switch").Name
+        $vm = New-VM -Name $vmName -NewVHDPath $vhdx -NewVHDSizeBytes 60GB -MemoryStartupBytes 4GB -Generation 2 -SwitchName "Default Switch"
         Set-VMProcessor $vm.Name -Count 2 -ExposeVirtualizationExtensions $true
-        Set-VMDvdDrive $vm.Name -Path (Read-Host "Path to Windows ISO").Trim('"')
+        Write-Output "Windows 11 download: https://www.microsoft.com/en-us/software-download/windows11"
+        $dvdDrive = Add-VMDvdDrive $vm.Name -Path (Read-Host "Path to Windows ISO").Trim('"') -Passthru
+        Set-VMFirmware $vm.Name -EnableSecureBoot On -BootOrder $dvdDrive, (Get-VMHardDiskDrive $vm.Name)
+        Set-VMKeyProtector $vm.Name -NewLocalKeyProtector
+        Enable-VMTPM $vm.Name
         Start-VM $vm
         Write-Output "Finish Windows install and return here before testing. Press Enter when ready."
         Start-Sleep -Seconds 3
