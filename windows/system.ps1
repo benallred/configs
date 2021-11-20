7b3d0f8390ef43d884bdc76ceafc7e75a075e66c
Ben Allred <ben@bmallred.com>
2021-11-20 16:14:41 -0700

    Windows: System: Match Windows 11 descriptions

--- windows/system.ps1
+++ windows/system.ps1
@@ -1,18 +1,18 @@
-Block "System > Power & sleep > Screen > On battery power, turn off after = 10 minutes" {
+Block "System > Power & battery > Screen and sleep > On battery power, turn off my screen after = 10 minutes" {
     powercfg /change monitor-timeout-dc 10
 }
-Block "System > Power & sleep > Screen > When plugged in, turn off after = 20 minutes" {
+Block "System > Power & battery > Screen and sleep > When plugged in, turn off my screen after = 20 minutes" {
     powercfg /change monitor-timeout-ac 20
 }
-Block "System > Power & sleep > Sleep > On battery power, PC goes to sleep after = 20 minutes" {
+Block "System > Power & battery > Screen and sleep > On battery power, put my device to sleep after = 20 minutes" {
     powercfg /change standby-timeout-dc 20
 }
 if ((Configured $forWork) -or (Configured $forKids)) {
-    Block "System > Power & sleep > Sleep > When plugged in, PC goes to sleep after = 60 minutes" {
+    Block "System > Power & battery > Screen and sleep > When plugged in, put my device to sleep after = 1 hour" {
         powercfg /change standby-timeout-ac 60
     }
 }
-Block "System > Power & sleep > Additional power settings > Choose what closing the lid does > When I close the lid (Plugged in) = Do nothing" {
+Block "Control Panel > Power Options > Choose what closing the lid does > When I close the lid [both On battery and Plugged in] = Do nothing" {
     # https://docs.microsoft.com/en-us/windows-hardware/customize/power-settings/power-button-and-lid-settings-lid-switch-close-action
     powercfg /SetACValueIndex SCHEME_BALANCED SUB_BUTTONS LIDACTION 0
     powercfg /SetDCValueIndex SCHEME_BALANCED SUB_BUTTONS LIDACTION 0
