ba2a8012776ba3ca29c192015d209d18d06f9a15
Ben Allred <ben@bmallred.com>
2021-11-20 16:31:10 -0700

    Windows: Devices: Match Windows 11 descriptions

--- windows/devices.ps1
+++ windows/devices.ps1
@@ -1,27 +1,27 @@
 if (!(Configured $forHome) -and !(Configured $forTest)) {
-    FirstRunBlock "Devices > Printers & scanners > Add a printer or scanner > The printer that I want isn't listed" {
+    FirstRunBlock "Bluetooth & devices > Printers & scanners > Add a printer or scanner > The printer that I want isn't listed" {
         Write-ManualStep "Select a shared printer by name = \\{Server}\{Printer}"
         rundll32 printui.dll PrintUIEntry /im
         WaitWhileProcess rundll32
     }
 }
-Block "Devices > Touchpad > Taps > Press the lower right corner of the touchpad to right-click = Off" {
+Block "Bluetooth & devices > Touchpad > Taps > Press the lower right corner of the touchpad to right-click = Off" {
     Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name RightClickZoneEnabled -Value 0
 } -RequiresReboot
-Block "Devices > Touchpad > Three-finger gestures > Taps = Middle mouse button" {
+Block "Bluetooth & devices > Touchpad > Three-finger gestures > Taps = Middle mouse button" {
     Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name ThreeFingerTapEnabled -Value 4
 } -RequiresReboot
-Block "Devices > Touchpad > Four-finger gestures" {
-    Write-Output "Devices > Touchpad > Four-finger gestures > Swipes = Custom"
+Block "Bluetooth & devices > Touchpad > Four-finger gestures" {
+    Write-Output "Bluetooth & devices > Touchpad > Four-finger gestures > Swipes = Custom"
     Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerSlideEnabled -Value 0xffff
-    Write-Output "Devices > Touchpad > Related settings > Advanced gesture configuration > Configure your four finger gestures > Up = Volume up"
-    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerUp -Value 16
-    Write-Output "Devices > Touchpad > Related settings > Advanced gesture configuration > Configure your four finger gestures > Down = Volume down"
-    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerDown -Value 17
-    Write-Output "Devices > Touchpad > Related settings > Advanced gesture configuration > Configure your four finger gestures > Left = Next track"
-    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerLeft -Value 14
-    Write-Output "Devices > Touchpad > Related settings > Advanced gesture configuration > Configure your four finger gestures > Right = Previous track"
-    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerRight -Value 15
-    Write-Output "Devices > Touchpad > Four-finger gestures > Taps = Play/pause"
+    Write-Output "Bluetooth & devices > Touchpad > Advanced gestures > Configure four-finger gestures > Tap = Play/pause"
     Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerTapEnabled -Value 3
+    Write-Output "Bluetooth & devices > Touchpad > Advanced gestures > Configure four-finger gestures > Swipe up = Volume up"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerUp -Value 16
+    Write-Output "Bluetooth & devices > Touchpad > Advanced gestures > Configure four-finger gestures > Swipe down = Volume down"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerDown -Value 17
+    Write-Output "Bluetooth & devices > Touchpad > Advanced gestures > Configure four-finger gestures > Swipe left = Next track"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerLeft -Value 14
+    Write-Output "Bluetooth & devices > Touchpad > Advanced gestures > Configure four-finger gestures > Swipe right = Previous track"
+    Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PrecisionTouchPad" -Name FourFingerRight -Value 15
 } -RequiresReboot
