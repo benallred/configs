3223fddf3eac831116b3f7518be4edc281a4dc45
Ben Allred <ben@bmallred.com>
2021-11-21 00:26:35 -0700

    Install: Visual Studio: Move to 2022 Community
    Microsoft.VisualStudio.Workload.NetCoreTools seems to have been removed in 2022 (perhaps it's default now)
    Try vanilla 2022 before installing extensions

--- install/install.ps1
+++ install/install.ps1
@@ -81,20 +81,19 @@ InstallFromWingetBlock Microsoft.VisualStudioCode {
 }
 
 if (!(Configured $forKids)) {
-    InstallFromWingetBlock Microsoft.VisualStudio.2019.Professional `
-        "--passive --norestart --wait --includeRecommended --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.VisualStudio.Workload.NetCoreTools" `
+    InstallFromWingetBlock Microsoft.VisualStudio.2022.Community `
+        "--passive --norestart --wait --includeRecommended --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb" `
     {
-        # https://docs.microsoft.com/en-us/visualstudio/install/workload-and-component-ids?view=vs-2019
+        # https://docs.microsoft.com/en-us/visualstudio/install/workload-and-component-ids
         # Microsoft.VisualStudio.Workload.ManagedDesktop    .NET desktop development
         # Microsoft.VisualStudio.Workload.NetWeb            ASP.NET and web development
-        # Microsoft.VisualStudio.Workload.NetCoreTools      .NET Core cross-platform development
-        # https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples?view=vs-2019#using---wait
+        # https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples#using---wait
 
-        InstallFollowup "Visual Studio" {
-            . (. "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -property productPath) $PSCommandPath
-            WaitWhile { !(Get-ChildItem "HKCU:\Software\Microsoft\VisualStudio" | ? { $_.PSChildName -match "^\d\d.\d_" }) } "Waiting for Visual Studio registry key"
-            & "$git\configs\programs\Visual Studio - Hide dynamic nodes in Solution Explorer.ps1"
-        }
+        # InstallFollowup "Visual Studio" {
+        #     . (. "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -property productPath) $PSCommandPath
+        #     WaitWhile { !(Get-ChildItem "HKCU:\Software\Microsoft\VisualStudio" | ? { $_.PSChildName -match "^\d\d.\d_" }) } "Waiting for Visual Studio registry key"
+        #     & "$git\configs\programs\Visual Studio - Hide dynamic nodes in Solution Explorer.ps1"
+        # }
 
         function InstallVisualStudioExtension([string]$Publisher, [string]$Extension) {
             $downloadUrl = (iwr "https://marketplace.visualstudio.com/items?itemName=$Publisher.$Extension" | sls "/_apis/public/gallery/publishers/$Publisher/vsextensions/$Extension/(\d+\.?)+/vspackage").Matches.Value | % { "https://marketplace.visualstudio.com$_" }
@@ -105,12 +104,12 @@ if (!(Configured $forKids)) {
             Start-Process $vsixInstaller $installArgs -Wait
         }
 
-        InstallVisualStudioExtension VisualStudioPlatformTeam SolutionErrorVisualizer
-        InstallVisualStudioExtension VisualStudioPlatformTeam FixMixedTabs
-        InstallVisualStudioExtension VisualStudioPlatformTeam PowerCommandsforVisualStudio
-        InstallVisualStudioExtension maksim-vorobiev PeasyMotion
-        InstallVisualStudioExtension JustinClareburtMSFT HotStatus
-        InstallVisualStudioExtension MadsKristensen ResetZoom
+        # InstallVisualStudioExtension VisualStudioPlatformTeam SolutionErrorVisualizer
+        # InstallVisualStudioExtension VisualStudioPlatformTeam FixMixedTabs
+        # InstallVisualStudioExtension VisualStudioPlatformTeam PowerCommandsforVisualStudio
+        # InstallVisualStudioExtension maksim-vorobiev PeasyMotion
+        # InstallVisualStudioExtension JustinClareburtMSFT HotStatus
+        # InstallVisualStudioExtension MadsKristensen ResetZoom
     }
 
     if (!(Configured $forTest)) {
