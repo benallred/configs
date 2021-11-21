a21a786c94d05bde318b69a7f53b2f658af4fe67
Ben Allred <ben@bmallred.com>
2021-11-20 22:50:57 -0700

    Windows: Time & Language: Match Windows 11 descriptions

--- windows/time-and-language.ps1
+++ windows/time-and-language.ps1
@@ -1,7 +1,7 @@
-Block "Time & Language > Date & time > Time zone = (UTC-07:00) Mountain Time (US & Canada)" {
+Block "Time & language > Date & time > Time zone = (UTC-07:00) Mountain Time (US & Canada)" {
     Set-TimeZone "Mountain Standard Time"
 }
-Block "Time & Language > Date & time > Add clocks for different time zones" {
+Block "Time & language > Date & time > Additional clocks" {
     function SetAdditionalClock([int]$ClockNumber, [string]$DisplayName, [string]$TimeZoneId) {
         Set-RegistryValue "HKCU:\Control Panel\TimeDate\AdditionalClocks\$ClockNumber" -Name Enable -Value 1
         Set-RegistryValue "HKCU:\Control Panel\TimeDate\AdditionalClocks\$ClockNumber" -Name DisplayName -Value $DisplayName
@@ -10,7 +10,7 @@ Block "Time & Language > Date & time > Add clocks for different time zones" {
     SetAdditionalClock 1 "UTC" "UTC"
     SetAdditionalClock 2 "Korea" "Korea Standard Time"
 }
-Block "Time & Language > Language > Keyboard > Input language hot keys > Between input languages > Change Key Sequence > Switch Input Language = Not Assigned" {
+Block "Time & language > Typing > Advanced keyboard settings > Input language hot keys > Between input languages > Change Key Sequence > Switch Input Language = Not Assigned" {
     Set-RegistryValue "HKCU:\Keyboard Layout\Toggle" -Name "Language Hotkey" -Value "3"
     Set-RegistryValue "HKCU:\Keyboard Layout\Toggle" -Name "Layout Hotkey" -Value "3"
 }
