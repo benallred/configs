763cb88961b97e84ff1a60c762d83070ab3898a7
Ben Allred <ben@bmallred.com>
2021-12-09 22:26:53 -0700

    Windows: Apps: Uninstall new default apps on Windows 11

--- windows/apps.ps1
+++ windows/apps.ps1
@@ -22,10 +22,14 @@ FirstRunBlock "Configure Mail" {
     ConfigureNotifications Mail
 }
 
+UninstallBlock Microsoft.BingNews_8wekyb3d8bbwe
 UninstallBlock Microsoft.BingWeather_8wekyb3d8bbwe
 UninstallBlock Microsoft.GetHelp_8wekyb3d8bbwe
 UninstallBlock Microsoft.Getstarted_8wekyb3d8bbwe # Tips
-UninstallBlock Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe
-UninstallBlock Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe
 UninstallBlock Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe
+UninstallBlock Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe
+UninstallBlock Microsoft.SkypeApp_kzf8qxf38zg5c
+UninstallBlock Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe
 UninstallBlock Microsoft.WindowsMaps_8wekyb3d8bbwe
+UninstallBlock Disney.37853FC22B2CE_6rarf9sa4v8jt # Disney+
+UninstallBlock SpotifyAB.SpotifyMusic_zpdnekdrzrea0
