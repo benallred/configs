e6be47eb452d238031368817bc7ee65c8c7a3a5f
Ben Allred <ben@bmallred.com>
2021-12-09 22:26:53 -0700

    Windows: Apps: Uninstall Mail and Calendar

--- windows/apps.ps1
+++ windows/apps.ps1
@@ -18,10 +18,6 @@ if (!(Configured $forKids)) {
     }
 }
 
-FirstRunBlock "Configure Mail" {
-    ConfigureNotifications Mail
-}
-
 UninstallBlock Microsoft.BingNews_8wekyb3d8bbwe
 UninstallBlock Microsoft.BingWeather_8wekyb3d8bbwe
 UninstallBlock Microsoft.GetHelp_8wekyb3d8bbwe
@@ -29,6 +25,7 @@ UninstallBlock Microsoft.Getstarted_8wekyb3d8bbwe # Tips
 UninstallBlock Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe
 UninstallBlock Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe
 UninstallBlock Microsoft.SkypeApp_kzf8qxf38zg5c
+UninstallBlock microsoft.windowscommunicationsapps_8wekyb3d8bbwe # Mail and Calendar
 UninstallBlock Microsoft.WindowsFeedbackHub_8wekyb3d8bbwe
 UninstallBlock Microsoft.WindowsMaps_8wekyb3d8bbwe
 UninstallBlock Disney.37853FC22B2CE_6rarf9sa4v8jt # Disney+
