8a2b517d6ceb840c229fc5140a04bcb60fa62ea8
Ben Allred <ben@bmallred.com>
2021-12-18 23:06:12 -0700

    Install: Battle.net: Configure

--- install/install.ps1
+++ install/install.ps1
@@ -282,7 +282,42 @@ InstallFromWingetBlock Valve.Steam {
 if (!(Configured $forWork)) {
     Block "Install Battle.net" {
         Download-File https://www.battle.net/download/getInstallerForGame $env:tmp\Battle.net-Setup.exe
+        $battleNetSettings = @{
+            Client = @{
+                AutoLogin = "true"
+                Install   = @{
+                    CreateDesktopShortcut = "false"
+                }
+            }
+        }
+        if (Configured $forHome) {
+            $battleNetSettings.Client.Install.DefaultInstallPath = "D:/Installs/Blizzard"
+        }
+        New-Item $env:AppData\Battle.net -Type Directory
+        ConvertTo-Json $battleNetSettings -Depth 10 | Set-Content $env:AppData\Battle.net\Battle.net.config
         . $env:tmp\Battle.net-Setup.exe
+        Add-Type -AssemblyName System.Windows.Forms
+        $monSize = [System.Windows.Forms.SystemInformation]::PrimaryMonitorSize
+        $battleNetUserSettings = @{
+            User = @{
+                Client = @{
+                    Windows                    = @{
+                        BrowserMainWindow = @{
+                            WindowPosition = "@Point($(($monSize.Width - 1000) / 2) $(($monSize.Height - 640) / 2))"
+                            WindowSize     = "@Size(1000 640)"
+                        }
+                    }
+                    HasSeenFirstTimeExperience = "true"
+                    HideOnClose                = "false"
+                    DisplayExitPrompt          = "false"
+                    WindowNotificationShown    = "true"
+                }
+            }
+        }
+        WaitWhile { !(dir $env:AppData\Battle.net -Exclude Battle.net.config) } "Waiting for Battle.net user settings"
+        $battleNetUserSettingsFilePath = dir $env:AppData\Battle.net -Exclude Battle.net.config
+        Stop-Process -Name Battle.net
+        ConvertTo-Json $battleNetUserSettings -Depth 10 | Set-Content $battleNetUserSettingsFilePath
         DeleteDesktopShortcut Battle.net
     } {
         Test-ProgramInstalled "Battle.net"
