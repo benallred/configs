366a3db6ce219510f707773dc89ee6246cb00c77
Ben Allred <ben@bmallred.com>
2021-12-28 09:39:04 -0700

    WIP

--- programs/Visual Studio - Hide dynamic nodes in Solution Explorer.ps1
+++ programs/Visual Studio - Hide dynamic nodes in Solution Explorer.ps1
@@ -1,2 +1,4 @@
 $visualStudioVersionKey = Get-ChildItem "HKCU:\Software\Microsoft\VisualStudio" | ? { $_.PSChildName -match "^\d\d.\d_" } | Select-Object -Last 1
 Set-ItemProperty Registry::$visualStudioVersionKey -Name UseSolutionNavigatorGraphProvider -Value 0
+
+$env:LocalAppData\Microsoft\VisualStudio\17.0_658d25fa\privateregistry.bin
