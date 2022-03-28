cb2a2628bf72fbdfa0bb23ddb402d9612c34d56d
Ben Allred <ben@bmallred.com>
2022-03-28 12:56:02 -0600

    PSReadLine: 2.2.2: Use new `ShowCommandHelp`
    https://docs.microsoft.com/en-us/powershell/module/psreadline/about/about_psreadline?view=powershell-7.2#showcommandhelp

--- powershell/PSReadLine.ps1
+++ powershell/PSReadLine.ps1
@@ -261,37 +261,3 @@ Set-PSReadLineKeyHandler -Key Backspace `
         }
     }
 }
-
-Set-PSReadLineKeyHandler -Key F1 `
-    -BriefDescription CommandHelp `
-    -LongDescription "Open the help window for the current command" `
-    -ScriptBlock {
-    param($key, $arg)
-
-    $ast = $null
-    $tokens = $null
-    $errors = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
-
-    $commandAst = $ast.FindAll( {
-            $node = $args[0]
-            $node -is [CommandAst] -and
-            $node.Extent.StartOffset -le $cursor -and
-            $node.Extent.EndOffset -ge $cursor
-        }, $true) | Select-Object -Last 1
-
-    if ($commandAst -ne $null) {
-        $commandName = $commandAst.GetCommandName()
-        if ($commandName -ne $null) {
-            $command = $ExecutionContext.InvokeCommand.GetCommand($commandName, 'All')
-            if ($command -is [AliasInfo]) {
-                $commandName = $command.ResolvedCommandName
-            }
-
-            if ($commandName -ne $null) {
-                Get-Help $commandName -Online # Ben
-            }
-        }
-    }
-}
