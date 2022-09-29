e23f16c1c2e603d0b8cb9cc8e3bf016b5d92f448
Ben Allred <ben@bmallred.com>
2022-09-29 16:47:24 -0600

    Template: github init: Create repo and link to other settings

--- templates/github-init.ps1
+++ templates/github-init.ps1
@@ -1,3 +1,15 @@
+$owner = Read-Host Owner
+$repo = $pwd | Split-Path -Leaf
+$workRepo = $owner -ne "benallred"
+gh repo create "$owner/$repo" ($workRepo ? "--private" : "--public") --source . --description (Read-Host Description)
+
 gh repo edit --enable-merge-commit=false
 gh repo edit --enable-squash-merge=false
 gh repo edit --delete-branch-on-merge
+
+Write-Host "When ready run ``git pushu``" -ForegroundColor Yellow
+
+if ($workRepo) {
+    Write-Output "$($PSStyle.Foreground.BrightYellow)Add team: $($PSStyle.Reset)https://github.com/$owner/$repo/settings/access"
+    Write-Output "$($PSStyle.Foreground.BrightYellow)Require linear history: $($PSStyle.Reset)https://github.com/$owner/$repo/settings/branch_protection_rules/new"
+}
