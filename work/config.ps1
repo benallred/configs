e9c99a48d40f9878a8353b2b2ba0783d78032e1b
Ben Allred <ben@bmallred.com>
2022-11-08 13:04:57 -0700

    Work: Reset

--- work/config.ps1
+++ work/config.ps1
@@ -14,12 +14,6 @@ if ((Configured $forWork) -or (Configured $forTest)) {
         Test-Path "C:\BenLocal\Programs\Tor Browser"
     }
 
-    Block "Uninstall Lenovo Quick Clean" {
-        winget uninstall "Lenovo Quick Clean"
-    } {
-        !(winget list "Lenovo Quick Clean" | sls "Lenovo Quick Clean")
-    }
-
     InstallFromWingetBlock 9WZDNCRFJBLK # Arc Touch Bluetooth Mouse
 
     InstallFromWingetBlock Zoom.Zoom {
@@ -44,50 +38,5 @@ if ((Configured $forWork) -or (Configured $forTest)) {
         Set-RegistryValue "HKLM:\SOFTWARE\Policies\Zoom\Zoom Meetings\General" -Name AutoJoinVoIP -Value 1
         Set-RegistryValue "HKLM:\SOFTWARE\Policies\Zoom\Zoom Meetings\General" -Name MuteVoIPWhenJoinMeeting -Value 1
         Set-RegistryValue "HKLM:\SOFTWARE\Policies\Zoom\Zoom Meetings\General" -Name EnterFullScreenWhenViewingSharedScreen -Value 0
-    }
-
-    InstallFromWingetBlock Microsoft.Teams {
-        DeleteDesktopShortcut "Microsoft Teams (work or school)"
-        # Teams does not seem to pick up manual changes to the settings file
-        # $teamsSettings = Get-Content $env:AppData\Microsoft\Teams\desktop-config.json | ConvertFrom-Json
-        # $teamsSettings | Add-Member NoteProperty theme "darkV2" -Force
-        # ConvertTo-Json $teamsSettings | Set-Content $env:AppData\Microsoft\Teams\desktop-config.json
-        Write-ManualStep "General > Theme = Dark"
-        Write-ManualStep "Privacy > Surveys = Off"
-    }
-
-    Block "Outlook > Options > Add-ins > Manage COM Add-ins > Mimecast for Outlook = Off" {
-        Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Office\Outlook\Addins\MimecastServicesForOutlook.AddinModule" -Name ADXStartMode -Value FIRSTSTART
-        Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Office\Outlook\Addins\MimecastServicesForOutlook.AddinModule" -Name LoadBehavior -Value 2
-    }
-
-    Block "Other Visual Studio Components" {
-        # Microsoft.VisualStudio.ComponentGroup.IISDevelopment    Development time IIS support
-        winget install --id Microsoft.VisualStudio.2022.Community --accept-package-agreements --override "--passive --norestart --wait --add Microsoft.VisualStudio.ComponentGroup.IISDevelopment"
-    } {
-        . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -format value -requires Microsoft.VisualStudio.ComponentGroup.IISDevelopment
-    }
-
-    InstallVisualStudioExtensionBlock TechTalkSpecFlowTeam SpecFlowForVisualStudio2022
-
-    InstallFromWingetBlock Amazon.AWSCLI {
-        Add-Content -Path $profile {
-            Register-ArgumentCompleter -Native -CommandName aws, awslocal -ScriptBlock {
-                param($wordToComplete, $commandAst, $cursorPosition)
-                $env:COMP_LINE = $commandAst
-                $env:COMP_POINT = $cursorPosition
-                . "$env:ProgramFiles\Amazon\AWSCLIV2\aws_completer.exe" | ForEach-Object {
-                    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-                }
-                Remove-Item Env:\COMP_LINE
-                Remove-Item Env:\COMP_POINT
-            }
-        }
-    }
-
-    InstallFromWingetBlock Microsoft.DotNet.SDK.5
-
-    Block "Set Elasticsearch Docker settings" {
-        Set-Content $env:UserProfile\.wslconfig "[wsl2]", "kernelCommandLine=sysctl.vm.max_map_count=262144"
     }
 }
