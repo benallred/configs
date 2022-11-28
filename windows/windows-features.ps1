20993a490bbbdbf3dc6ec276391065e900865f38
Ben Allred <ben@bmallred.com>
2022-11-28 13:15:20 -0700

    Windows: Features: Stop enabling IIS

--- windows/windows-features.ps1
+++ windows/windows-features.ps1
@@ -10,8 +10,6 @@ function WindowsFeatureBlock([string]$Comment, [string]$FeatureName) {
 WindowsFeatureBlock ".NET Framework 3.5 (includes .NET 2.0 and 3.0)" NetFx3
 
 if (!(Configured $forKids)) {
-    WindowsFeatureBlock "Internet Information Services > Web Management Tools > IIS Management Console" IIS-ManagementConsole
-    WindowsFeatureBlock "Internet Information Services > World Wide Web Services > Application Development Features > ASP.NET 4.x" IIS-ASPNET45
     WindowsFeatureBlock "Hyper-V" Microsoft-Hyper-V
 
     Block "Install WSL" {
