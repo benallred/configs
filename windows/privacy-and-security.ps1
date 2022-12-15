55aa69eaed0f1cc982c61f5aa3cdef71567af509
Ben Allred <ben@bmallred.com>
2022-12-15 09:28:25 -0700

    Windows: Privacy & Security: BitLocker

--- windows/privacy-and-security.ps1
+++ windows/privacy-and-security.ps1
@@ -1,3 +1,7 @@
+FirstRunBlock "Privacy & security > Device encryption > BitLocker drive encryption > Back up your recovery key" {
+    $recoveryKey = (Get-BitLockerVolume).KeyProtector | ? { $_.KeyProtectorType -eq "RecoveryPassword" }
+    Add-Content "$OneDrive\BitLocker Recovery Key ($env:ComputerName) $($recoveryKey.KeyProtectorId.Trim('{', '}')).txt" $recoveryKey.RecoveryPassword
+}
 Block "Privacy & security > For developers > Change settings to allow remote connections to this computer > Show settings > Allow Remote Assistance connections to this computer = Off" {
     Set-RegistryValue "HKLM:\SYSTEM\CurrentControlSet\Control\Remote Assistance" -Name fAllowToGetHelp -Value 0
     Disable-NetFirewallRule -DisplayGroup "Remote Assistance"
