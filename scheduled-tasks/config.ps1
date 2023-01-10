4cb993f4317c5dc340c57e4ead19a9175536b953
Ben Allred <ben@bmallred.com>
2023-01-10 12:56:19 -0700

    Scheduled Task: Run as admin

--- scheduled-tasks/config.ps1
+++ scheduled-tasks/config.ps1
@@ -1,7 +1,7 @@
 function ScheduledTaskBlock([Parameter(Mandatory)][string]$Comment, [Parameter(Mandatory)][string]$ScriptFilePath, [Parameter(Mandatory)][string]$TaskName, [DateTime]$Time) {
     Block $Comment {
         $at = if ($Time) { $Time } else { "07:00" }
-        $action = New-ScheduledTaskAction -Execute (Get-Command wt).Source -Argument "-w scheduled-tasks pwsh `"$ScriptFilePath`""
+        $action = New-ScheduledTaskAction -Execute (Get-Command wt).Source -Argument "-w scheduled-tasks pwsh -Command `"Start-AsAdmin '$ScriptFilePath'`""
         $trigger = New-ScheduledTaskTrigger -Daily -At $at
         $settingsSet = New-ScheduledTaskSettingsSet -StartWhenAvailable
         $task = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settingsSet
