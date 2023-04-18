ff995b1fc48c8503e5ab448cbc9d6bef611bf6f9
Ben Allred <ben@bmallred.com>
2023-04-18 16:26:31 -0600

    Scheduled Task: Work: Daily: Randomize VCM image

--- scheduled-tasks/work/daily.ps1
+++ scheduled-tasks/work/daily.ps1
@@ -4,4 +4,11 @@ git config --global --unset user.email
 
 & $PSScriptRoot\..\delete-desktop-shortcuts.ps1
 & $PSScriptRoot\..\prune-transcripts.ps1
-& $emConfigs\em-config.ps1
+StopOnError { & $emConfigs\em-config.ps1 }
+
+StopOnError {
+    Stop-Process -Name PowerToys
+    WaitWhileProcess PowerToys
+    Copy-Item (Get-Random (Get-ChildItem "$env:OneDrive\Ben\Settings\Backgrounds - Work\Despair, Inc")) C:\BenLocal\VCM_Random.jpg
+    . "$env:ProgramFiles\PowerToys\PowerToys.exe"
+}
