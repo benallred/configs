a2963bf318ab68ae60007d537defc2aced3fd0f6
Ben Allred <ben@bmallred.com>
2023-07-23 21:48:03 -0600

    Scheduled Task: Delete desktop shortcuts: Also delete files with .url extensions

--- scheduled-tasks/delete-desktop-shortcuts.ps1
+++ scheduled-tasks/delete-desktop-shortcuts.ps1
@@ -2,6 +2,6 @@ Write-Output ("*" * $Host.UI.RawUI.WindowSize.Width)
 Write-Output "Deleting desktop shortcuts"
 Get-Content C:\BenLocal\.delete-desktop-shortcuts.txt | % {
     Write-Output "`t$_"
-    Remove-Item "$env:Public\Desktop\$_.lnk" -ErrorAction Ignore
-    Remove-Item "$env:UserProfile\Desktop\$_.lnk" -ErrorAction Ignore
+    Remove-Item "$env:Public\Desktop\$_.*" -ErrorAction Ignore
+    Remove-Item "$env:UserProfile\Desktop\$_.*" -ErrorAction Ignore
 }
