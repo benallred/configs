8bbf2c34a92c702a9db9ee5a808d53f28f0e30c0
Ben Allred <ben@bmallred.com>
2023-12-28 15:27:02 -0700

    WIP

--- scheduled-tasks/home/daily.ps1
+++ scheduled-tasks/home/daily.ps1
@@ -11,6 +11,9 @@ StopOnError 8 {
     dotnet run --project $git\DilbertImageDownloader\ -- --save-folder=$saveFolder
     robocopy $saveFolder C:\Ben\Desktop\Miguk\Dilbert /S /MAXAGE:1
 }
-StopOnError { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLAYgY8SPtEWGh243j2fmgeCxnFtpbWwZd --download-folder "E:\Media\Church\TV\Book of Mormon Videos" }
-StopOnError { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ-OuDJlytqoxj5oEme6RSVz --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 0 }
-StopOnError { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ8pQN7WMrWqWyqlsFlzbl-- --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 12 }
+# StopOnError { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLAYgY8SPtEWGh243j2fmgeCxnFtpbWwZd --download-folder "E:\Media\Church\TV\Book of Mormon Videos" }
+# StopOnError { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ-OuDJlytqoxj5oEme6RSVz --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 0 }
+# StopOnError { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLcnZqvqjU4U1dSZs-DUoh4hm9Dil_rXQC --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 13 }
+# StopOnError { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ94Emxl7cxlAXfq3CfabhW4 --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 14 }
+# StopOnError { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ98VLS8yGvHxXKeiIeYNzUo --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 15 }
+# StopOnError { dotnet run --project $git\YouTubeToPlex\YouTubeToPlex\ -- playlist --id PLGVpxD1HlmJ_MACuBYpU2K2qqHCoQJ7c4 --download-folder "E:\Media\Ben\TV\Family TV\Studio C" --season 16 }
