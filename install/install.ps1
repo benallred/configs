000f0e9e14c1dcd1e9607dda41835ae9e9507520
Ben Allred <ben@bmallred.com>
2025-11-02 22:30:43 -0700

    Install: Move system utilities into system-utils.ps1

--- install/install.ps1
+++ install/install.ps1
@@ -35,13 +35,6 @@ if (Configured $forHome, $forWork, $forTest) {
     }
 }
 
-InstallFromWingetBlock voidtools.Everything {
-    DeleteDesktopShortcut Everything
-    Copy-Item $PSScriptRoot\..\programs\Everything.ini $env:ProgramFiles\Everything\
-    . $env:ProgramFiles\Everything\Everything.exe -install-run-on-system-startup
-    . $env:ProgramFiles\Everything\Everything.exe -startup
-}
-
 if (Configured $forHome, $forWork, $forTest) {
     InstallFromWingetBlock SlackTechnologies.Slack {
         . $env:ProgramFiles\Slack\slack.exe
@@ -151,33 +144,7 @@ Block "Configure Office" {
     Set-RegistryValue "HKCU:\SOFTWARE\Microsoft\Office\16.0\OneNote\Options\Other" -Name NavigationBarExpColState -Value 1
 }
 
-InstallFromScoopBlock sysinternals {
-    Set-RegistryValue "HKCU:\Software\Sysinternals" EulaAccepted 1
-}
-
-InstallFromGitHubBlock benallred SnapX
-Block "Start SnapX" {
-    . $git\SnapX\SnapX.ahk
-} {
-    (Get-Process AutoHotkey -ErrorAction Ignore).CommandLine | sls SnapX.ahk
-}
-
 if (Configured $forHome, $forWork, $forTest) {
-    InstallFromGitHubBlock benallred Bahk
-    Block "Start Bahk" {
-        . $git\Bahk\Ben.ahk
-    } {
-        (Get-Process AutoHotkey -ErrorAction Ignore).CommandLine | sls Ben.ahk
-    }
-
-    if (Configured $forHome) {
-        InstallFromWingetBlock CrashPlan.CrashPlanSMB {
-            Write-ManualStep "Sign in"
-            Write-ManualStep "Replace Existing"
-            Write-ManualStep "Skip File Transfer"
-        }
-    }
-
     InstallFromGitHubBlock benallred plex-playlist-liberator
 
     InstallFromGitHubBlock benallred YouTubeToPlex
@@ -200,24 +167,6 @@ if (Configured $forHome, $forWork, $forTest) {
     } -NoUpdate
 }
 
-InstallFromWingetBlock 7zip.7zip {
-    Set-RegistryValue "HKCU:\SOFTWARE\7-Zip\FM" -Name ShowDots -Value 1
-    Set-RegistryValue "HKCU:\SOFTWARE\7-Zip\FM" -Name ShowRealFileIcons -Value 1
-    Set-RegistryValue "HKCU:\SOFTWARE\7-Zip\FM" -Name FullRow -Value 1
-    Set-RegistryValue "HKCU:\SOFTWARE\7-Zip\FM" -Name ShowSystemMenu -Value 1
-    . "$env:ProgramFiles\7-Zip\7zFM.exe"
-    Write-ManualStep "Tools >"
-    Write-ManualStep "`tOptions >"
-    Write-ManualStep "`t`t7-Zip >"
-    Write-ManualStep "`t`t`tContext menu items > [only the following]"
-    Write-ManualStep "`t`t`t`tOpen archive"
-    Write-ManualStep "`t`t`t`tExtract Here"
-    Write-ManualStep "`t`t`t`tExtract to <Folder>"
-    Write-ManualStep "`t`t`t`tAdd to <Archive>.zip"
-    Write-ManualStep "`t`t`t`tCRC SHA >"
-    WaitWhileProcess 7zFM
-}
-
 if (Configured $forHome) {
     FirstRunBlock "Wait for Plex backup restore" {
         WaitForPath "HKCU:\SOFTWARE\PlexPlaylistLiberator"
@@ -235,9 +184,9 @@ if (!(Configured $forHtpc)) {
     } -NoUpdate
 }
 
-InstallFromWingetBlock 9NBLGGH1ZBKW # Dynamic Theme
-
 & $PSScriptRoot\devices.ps1
+
+& $PSScriptRoot\system-utils.ps1
 
 if (Configured $forHome, $forWork, $forTest) {
     InstallFromWingetBlock Doist.Todoist {
@@ -271,17 +220,6 @@ if (Configured $forHome, $forWork, $forTest) {
         New-Shortcut (Get-ChildItem "$env:LocalAppData\Microsoft\WinGet\Packages\Rufus.Rufus_Microsoft.Winget.Source_8wekyb3d8bbwe" rufus*.exe) "$env:AppData\Microsoft\Windows\Start Menu\Programs\Ben\Rufus.lnk"
     }
 
-    Block "Install RegFromApp" {
-        Download-File https://www.nirsoft.net/utils/regfromapp-x64.zip $env:tmp\regfromapp-x64.zip
-        Expand-Archive $env:tmp\regfromapp-x64.zip C:\BenLocal\Programs\RegFromApp64
-        New-Shortcut C:\BenLocal\Programs\RegFromApp64\RegFromApp.exe "$env:AppData\Microsoft\Windows\Start Menu\Programs\Ben\RegFromApp64.lnk"
-        Download-File https://www.nirsoft.net/utils/regfromapp.zip $env:tmp\regfromapp.zip
-        Expand-Archive $env:tmp\regfromapp.zip C:\BenLocal\Programs\RegFromApp
-        New-Shortcut C:\BenLocal\Programs\RegFromApp\RegFromApp.exe "$env:AppData\Microsoft\Windows\Start Menu\Programs\Ben\RegFromApp.lnk"
-    } {
-        Test-Path C:\BenLocal\Programs\RegFromApp64
-    }
-
     if (Configured $forHome) {
         Block "Install OverDrive" {
             Download-File https://static.od-cdn.com/ODMediaConsoleSetup.msi $env:tmp\ODMediaConsoleSetup.msi
@@ -291,52 +229,6 @@ if (Configured $forHome, $forWork, $forTest) {
         } {
             Test-ProgramInstalled "OverDrive for Windows"
         }
-    }
-
-    InstallFromWingetBlock NickeManarin.ScreenToGif {
-        DeleteDesktopShortcut ScreenToGif
-        Copy-Item2 $PSScriptRoot\..\programs\ScreenToGif.xaml $env:AppData\ScreenToGif\Settings.xaml
-    }
-
-    function DownloadAndExtractVeraCrypt([string]$Version, [string]$TargetDir) {
-        $downloadPath = "$veraCryptRootDir\VeraCrypt Portable $Version.exe"
-        $versionDir = "$veraCryptRootDir\$Version"
-
-        Download-File https://launchpad.net/veracrypt/trunk/$Version/+download/VeraCrypt%20Portable%20$Version.exe $downloadPath
-        mkdir $versionDir | Out-Null
-        $TargetDir | Set-Clipboard
-        Write-ManualStep "Extract to `"$TargetDir`" (copied to clipboard)"
-        start $veraCryptRootDir
-        . $downloadPath
-        WaitWhileProcess *VeraCrypt*
-    }
-
-    Block "Install VeraCrypt" {
-        $version = (winget show IDRIX.VeraCrypt | sls "(?<=Version: ).*").Matches.Value
-        $currentDir = "$veraCryptRootDir\Current"
-
-        DownloadAndExtractVeraCrypt $version $currentDir
-        New-Shortcut $currentDir\VeraCrypt-x64.exe "$env:AppData\Microsoft\Windows\Start Menu\Programs\Ben\VeraCrypt.lnk"
-    } {
-        Test-Path "$veraCryptRootDir\Current"
-    } {
-        $script:veraCryptOldVersion = Get-ChildItem $veraCryptRootDir -Directory -Exclude Current | sort Name | select -Last 1 | Split-Path -Leaf
-        $script:veraCryptNewVersion = (winget show IDRIX.VeraCrypt | sls "(?<=Version: ).*").Matches.Value
-        Write-Host "Old VeraCrypt version: $script:veraCryptOldVersion"
-        Write-Host "New VeraCrypt version: $script:veraCryptNewVersion"
-        $script:veraCryptNewVersion -ne $script:veraCryptOldVersion
-    } {
-        $currentDir = "$veraCryptRootDir\Current"
-
-        rm $veraCryptRootDir\$script:veraCryptOldVersion
-        Rename-Item $currentDir $script:veraCryptOldVersion
-        DownloadAndExtractVeraCrypt $script:veraCryptNewVersion $currentDir
-    }
-
-    InstallFromWingetBlock Microsoft.PowerToys {
-        Copy-Item2 $PSScriptRoot\..\programs\PowerToys.settings.json $env:LocalAppData\Microsoft\PowerToys\settings.json
-        Copy-Item2 $PSScriptRoot\..\programs\PowerToys.VideoConference.settings.json "$env:LocalAppData\Microsoft\PowerToys\Video Conference\settings.json"
-        Copy-Item2 $PSScriptRoot\..\programs\PowerToys.ColorPicker.settings.json "$env:LocalAppData\Microsoft\PowerToys\ColorPicker\settings.json"
     }
 
     Block "Install nanDECK" {
@@ -356,8 +248,6 @@ if (!(Configured $forHtpc)) {
 InstallFromWingetBlock VideoLAN.VLC {
     DeleteDesktopShortcut "VLC media player"
 }
-
-InstallFromWingetBlock JAMSoftware.TreeSize.Free
 
 if (Configured $forKids) {
     InstallFromWingetBlock MITMediaLab.Scratch.3 {
---
+++ install/system-utils.ps1
@@ -0,0 +1,113 @@
+InstallFromGitHubBlock benallred SnapX
+Block "Start SnapX" {
+    . $git\SnapX\SnapX.ahk
+} {
+    (Get-Process AutoHotkey -ErrorAction Ignore).CommandLine | sls SnapX.ahk
+}
+
+InstallFromWingetBlock voidtools.Everything {
+    DeleteDesktopShortcut Everything
+    Copy-Item $PSScriptRoot\..\programs\Everything.ini $env:ProgramFiles\Everything\
+    . $env:ProgramFiles\Everything\Everything.exe -install-run-on-system-startup
+    . $env:ProgramFiles\Everything\Everything.exe -startup
+}
+
+InstallFromWingetBlock 7zip.7zip {
+    Set-RegistryValue "HKCU:\SOFTWARE\7-Zip\FM" -Name ShowDots -Value 1
+    Set-RegistryValue "HKCU:\SOFTWARE\7-Zip\FM" -Name ShowRealFileIcons -Value 1
+    Set-RegistryValue "HKCU:\SOFTWARE\7-Zip\FM" -Name FullRow -Value 1
+    Set-RegistryValue "HKCU:\SOFTWARE\7-Zip\FM" -Name ShowSystemMenu -Value 1
+    . "$env:ProgramFiles\7-Zip\7zFM.exe"
+    Write-ManualStep "Tools >"
+    Write-ManualStep "`tOptions >"
+    Write-ManualStep "`t`t7-Zip >"
+    Write-ManualStep "`t`t`tContext menu items > [only the following]"
+    Write-ManualStep "`t`t`t`tOpen archive"
+    Write-ManualStep "`t`t`t`tExtract Here"
+    Write-ManualStep "`t`t`t`tExtract to <Folder>"
+    Write-ManualStep "`t`t`t`tAdd to <Archive>.zip"
+    Write-ManualStep "`t`t`t`tCRC SHA >"
+    WaitWhileProcess 7zFM
+}
+
+InstallFromWingetBlock 9NBLGGH1ZBKW # Dynamic Theme
+
+InstallFromWingetBlock JAMSoftware.TreeSize.Free
+
+InstallFromScoopBlock sysinternals {
+    Set-RegistryValue "HKCU:\Software\Sysinternals" EulaAccepted 1
+}
+
+if (Configured $forHome) {
+    InstallFromWingetBlock CrashPlan.CrashPlanSMB {
+        Write-ManualStep "Sign in"
+        Write-ManualStep "Replace Existing"
+        Write-ManualStep "Skip File Transfer"
+    }
+}
+
+if (Configured $forHome, $forWork, $forTest) {
+    InstallFromGitHubBlock benallred Bahk
+    Block "Start Bahk" {
+        . $git\Bahk\Ben.ahk
+    } {
+        (Get-Process AutoHotkey -ErrorAction Ignore).CommandLine | sls Ben.ahk
+    }
+
+    InstallFromWingetBlock Microsoft.PowerToys {
+        Copy-Item2 $PSScriptRoot\..\programs\PowerToys.settings.json $env:LocalAppData\Microsoft\PowerToys\settings.json
+        Copy-Item2 $PSScriptRoot\..\programs\PowerToys.VideoConference.settings.json "$env:LocalAppData\Microsoft\PowerToys\Video Conference\settings.json"
+        Copy-Item2 $PSScriptRoot\..\programs\PowerToys.ColorPicker.settings.json "$env:LocalAppData\Microsoft\PowerToys\ColorPicker\settings.json"
+    }
+
+    Block "Install RegFromApp" {
+        Download-File https://www.nirsoft.net/utils/regfromapp-x64.zip $env:tmp\regfromapp-x64.zip
+        Expand-Archive $env:tmp\regfromapp-x64.zip C:\BenLocal\Programs\RegFromApp64
+        New-Shortcut C:\BenLocal\Programs\RegFromApp64\RegFromApp.exe "$env:AppData\Microsoft\Windows\Start Menu\Programs\Ben\RegFromApp64.lnk"
+        Download-File https://www.nirsoft.net/utils/regfromapp.zip $env:tmp\regfromapp.zip
+        Expand-Archive $env:tmp\regfromapp.zip C:\BenLocal\Programs\RegFromApp
+        New-Shortcut C:\BenLocal\Programs\RegFromApp\RegFromApp.exe "$env:AppData\Microsoft\Windows\Start Menu\Programs\Ben\RegFromApp.lnk"
+    } {
+        Test-Path C:\BenLocal\Programs\RegFromApp64
+    }
+
+    function DownloadAndExtractVeraCrypt([string]$Version, [string]$TargetDir) {
+        $downloadPath = "$veraCryptRootDir\VeraCrypt Portable $Version.exe"
+        $versionDir = "$veraCryptRootDir\$Version"
+
+        Download-File https://launchpad.net/veracrypt/trunk/$Version/+download/VeraCrypt%20Portable%20$Version.exe $downloadPath
+        mkdir $versionDir | Out-Null
+        $TargetDir | Set-Clipboard
+        Write-ManualStep "Extract to `"$TargetDir`" (copied to clipboard)"
+        start $veraCryptRootDir
+        . $downloadPath
+        WaitWhileProcess *VeraCrypt*
+    }
+
+    Block "Install VeraCrypt" {
+        $version = (winget show IDRIX.VeraCrypt | sls "(?<=Version: ).*").Matches.Value
+        $currentDir = "$veraCryptRootDir\Current"
+
+        DownloadAndExtractVeraCrypt $version $currentDir
+        New-Shortcut $currentDir\VeraCrypt-x64.exe "$env:AppData\Microsoft\Windows\Start Menu\Programs\Ben\VeraCrypt.lnk"
+    } {
+        Test-Path "$veraCryptRootDir\Current"
+    } {
+        $script:veraCryptOldVersion = Get-ChildItem $veraCryptRootDir -Directory -Exclude Current | sort Name | select -Last 1 | Split-Path -Leaf
+        $script:veraCryptNewVersion = (winget show IDRIX.VeraCrypt | sls "(?<=Version: ).*").Matches.Value
+        Write-Host "Old VeraCrypt version: $script:veraCryptOldVersion"
+        Write-Host "New VeraCrypt version: $script:veraCryptNewVersion"
+        $script:veraCryptNewVersion -ne $script:veraCryptOldVersion
+    } {
+        $currentDir = "$veraCryptRootDir\Current"
+
+        rm $veraCryptRootDir\$script:veraCryptOldVersion
+        Rename-Item $currentDir $script:veraCryptOldVersion
+        DownloadAndExtractVeraCrypt $script:veraCryptNewVersion $currentDir
+    }
+
+    InstallFromWingetBlock NickeManarin.ScreenToGif {
+        DeleteDesktopShortcut ScreenToGif
+        Copy-Item2 $PSScriptRoot\..\programs\ScreenToGif.xaml $env:AppData\ScreenToGif\Settings.xaml
+    }
+}
